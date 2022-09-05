fb5cd77301675b52cc0c6ada4f38d5335ae75829
anamulriyad <111343750+anamulriyad@users.noreply.github.com>
2022-09-05 13:58:05 +0600

    Add files via upload

--- main.c
+++ main.c
@@ -11,6 +11,8 @@ float balance;
 int main()
 {
     struct user usr;
+    FILE *fp;
+    char filename[50],phone[50],pword[50];
     int opt;
 
 
@@ -23,13 +25,46 @@ int main()
 
     if(opt==1){
         system("clear");
-        printf("Enter your account no:\t");
+        printf("Enter your account number:\t");
         scanf("%s",usr.ac);
-        printf(" Enter your phone no:\t");
+        printf(" Enter your phone number:\t");
         scanf("%s",usr.phone);
         printf(" Enter your new password:\t");
         scanf("%s",usr.password);
         usr.balance=0;
+        strcpy(filename,usr.phone);
+        fp=fopen(strcat(filename,".dat"),"w");
+        fwrite(&usr,sizeof(struct user),1,fp);
+        if(fwrite!=0){
+            printf("\n\n Account successfully registered ");
+        }else{
+        printf("\n\n something went wrong please try again");
+        }
+        fclose(fp);
     }
+    if("opt==2"){
+        system("clear");
+        printf("\n phone number:\t");
+        scanf("%s",phone);
+        printf("password:\t");
+        scanf("%s",pword);
+        strcpy(filename,phone);
+        fp=fopen(strcat(filename,".dat"),"r");
+        if(fp==NULL){
+            printf("\n Account number not registered");
+        }
+        else{
+        fread(&usr,sizeof(struct user),1,fp);
+        fclose(fp);
+        if(!strcmp(pword,usr.password)){
+            printf("\n password matched");
+        }
+        else{
+            printf("\n invalid password");
+        }
+    }
+    }
+
+
     return 0;
 }
